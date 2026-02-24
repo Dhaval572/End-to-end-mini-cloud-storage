adbab557d69c942063d24cb23c1ddf986e0ada62
Dhaval572 <dp9718776@example.com>
2026-02-25 01:59:53 +0530

    Update

--- client/main.cpp
+++ client/main.cpp
@@ -2,9 +2,11 @@
 #include "ftxui/screen/screen.hpp"
 #include "ftxui/component/component.hpp"
 #include "ftxui/component/screen_interactive.hpp"
+#include "ftxui/component/component_options.hpp"
 #include <iostream>
 #include <string>
 #include <vector>
+#include <fstream>
 #include <httplib.h>
 
 class Client
@@ -14,11 +16,13 @@ private:
     std::string current_user;
     std::string server_url;
     std::vector<std::string> file_list;
+    std::string status_message;
 
 public:
     Client(const std::string& server_address = "localhost", int port = 8080)
         : server_url("http://" + server_address + ":" + std::to_string(port))
         , http_client(server_url)
+        , status_message("Ready")
     {
     }
 
@@ -31,24 +35,58 @@ public:
         // Login screen
         std::string username, password;
         bool login_success = false;
+        std::string login_status = "";
         
-        auto login_component = Container::Vertical({
-            Input(&username, "Username"),
-            Input(&password, "Password", true),
-            Button("Login", [this, &username, &password, &login_success, &screen]{
-                if (Login(username, password)) {
-                    login_success = true;
-                    screen.Exit();
-                }
-            }),
-            Button("Register", [this, &username, &password, &screen]{
-                if (Register(username, password)) {
-                    std::cout << "Registration successful!" << std::endl;
-                }
-            })
+        // Create input components
+        auto username_input = Input(&username, "Username");
+        auto password_input = Input(&password, "Password");
+        
+        // Make password field
+        password_input |= CatchEvent([&](Event event) {
+            return false;  // Let FTXUI handle normally
         });
         
-        screen.Loop(login_component);
+        // Login button
+        auto login_button = Button("Login", [&] {
+            if (Login(username, password)) {
+                login_success = true;
+                screen.ExitLoopClosure()();
+            } else {
+                login_status = "Login failed";
+            }
+        });
+        
+        // Register button
+        auto register_button = Button("Register", [&] {
+            if (Register(username, password)) {
+                login_status = "Registration successful! Please login.";
+            } else {
+                login_status = "Registration failed";
+            }
+        });
+        
+        // Login screen layout
+        auto login_components = Container::Vertical({
+            username_input,
+            password_input,
+            Container::Horizontal({login_button, register_button})
+        });
+        
+        auto login_renderer = Renderer(login_components, [&] {
+            return vbox({
+                text("E2Eye Login") | bold | center,
+                separator(),
+                text("Username:"),
+                username_input->Render(),
+                text("Password:"),
+                password_input->Render(),
+                separator(),
+                hbox(login_button->Render(), register_button->Render()),
+                text(login_status) | color(Color::GrayDark)
+            }) | border;
+        });
+        
+        screen.Loop(login_renderer);
         
         if (login_success) {
             ShowMainMenu(screen);
@@ -87,80 +125,90 @@ private:
         using namespace ftxui;
         
         int selected_tab = 0;
-        std::vector<std::string> tabs = {"Files", "Upload", "Storage"};
+        std::vector<std::string> tab_titles = {"Files", "Upload", "Storage"};
         
-        auto tab_selection = Menu(&tabs, &selected_tab);
+        // Tab menu
+        auto tab_menu = Menu(&tab_titles, &selected_tab);
         
-        auto main_component = Container::Tab
-        ({
-            // Files tab
-            Renderer([this]{
-                RefreshFileList();
-                using namespace ftxui;
-                Elements file_elements;
-                for (const auto& file : file_list) 
-                {
-                    file_elements.push_back(text(file));
-                }
-                return vbox({
-                    text("Your Files:"),
-                    separator(),
-                    vbox(file_elements)
-                });
-            }),
-            
-            // Upload tab
-            Renderer
-            (
-                [this]
-                {
-                    using namespace ftxui;
-                    return vbox
-                    (
-                        {
-                            text("Upload File:"),
-                            separator(),
-                            text("Use command line for file upload")
-                        }
-                    );
-                }
-            ),
-            
-            // Storage tab
-            Renderer
-            (
-                [this]
-                {
-                    auto storage_info = GetStorageInfo();
-                    using namespace ftxui;
-                    return vbox
-                    (
-                        {
-                            text("Storage Information:"),
-                            separator(),
-                            text(storage_info)
-                        }
-                    );
-                }
-            )
-        }, &selected_tab);
-        
-        auto layout = Container::Vertical
-        (
+        // Files tab content
+        auto files_tab = Renderer([&] {
+            RefreshFileList();
+            Elements file_elements;
+            for (const auto& file : file_list) 
             {
-                tab_selection,
-                main_component
+                file_elements.push_back(text("  " + file));
             }
+            if (file_elements.empty()) {
+                file_elements.push_back(text("  No files found"));
+            }
+            return vbox({
+                text("Your Files:") | bold,
+                separator(),
+                vbox(file_elements) | frame | size(HEIGHT, LESS_THAN, 20),
+                text("") | size(HEIGHT, EQUAL, 1),
+                text(status_message) | color(Color::Blue)
+            }) | border;
+        });
+        
+        // Upload tab content
+        auto upload_tab = Renderer([&] {
+            return vbox({
+                text("Upload File:") | bold,
+                separator(),
+                text("Use command line to upload files:"),
+                text(""),
+                text("  curl -X POST http://" + server_url + "/upload \\"),
+                text("    -F \"username=" + current_user + "\" \\"),
+                text("    -F \"filename=yourfile.jpg\" \\"),
+                text("    -F \"file=@/path/to/yourfile.jpg\""),
+                text(""),
+                text("Or use the test script:")
+            }) | border;
+        });
+        
+        // Storage tab content
+        auto storage_tab = Renderer([&] {
+            std::string storage_info = GetStorageInfo();
+            return vbox({
+                text("Storage Information:") | bold,
+                separator(),
+                text(storage_info),
+                text(""),
+                text("Refresh to update")
+            }) | border;
+        });
+        
+        // Container for tabs
+        auto tab_container = Container::Tab(
+            {
+                files_tab,
+                upload_tab,
+                storage_tab
+            },
+            &selected_tab
         );
         
-        screen.Loop(layout);
+        // Main layout
+        auto main_layout = Container::Vertical({
+            tab_menu,
+            tab_container
+        });
+        
+        auto main_renderer = Renderer(main_layout, [&] {
+            return vbox({
+                text("E2Eye - User: " + current_user) | bold | center,
+                separator(),
+                tab_menu->Render() | hcenter,
+                separator(),
+                tab_container->Render() | flex,
+            }) | border;
+        });
+        
+        screen.Loop(main_renderer);
     }
 
     void RefreshFileList()
     {
-        httplib::Params params;
-        params.emplace("username", current_user);
-        
         std::string url = "/files?username=" + current_user;
         auto response = http_client.Get(url.c_str());
         
@@ -178,14 +226,16 @@ private:
                 }
                 content.erase(0, pos + 1);
             }
+            status_message = "Found " + std::to_string(file_list.size()) + " files";
+        }
+        else
+        {
+            status_message = "Failed to load files";
         }
     }
 
     std::string GetStorageInfo()
     {
-        httplib::Params params;
-        params.emplace("username", current_user);
-        
         std::string url = "/storage?username=" + current_user;
         auto response = http_client.Get(url.c_str());
         
@@ -211,8 +261,16 @@ int main(int argc, char* argv[])
         port = std::stoi(argv[2]);
     }
     
-    Client client(server_address, port);
-    client.Run();
+    try
+    {
+        Client client(server_address, port);
+        client.Run();
+    }
+    catch (const std::exception& e)
+    {
+        std::cerr << "Error: " << e.what() << std::endl;
+        return 1;
+    }
     
     return 0;
 }
